ce271bcf2bd2f5e8be4c30f8489606e7b7accba6
Emirle <e.lepisto@hotmail.com>
2017-01-30 21:00:11 +0200

    Hups

--- src/client/Kirjautuminen.java
+++ src/client/Kirjautuminen.java
@@ -3,53 +3,61 @@ package client;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
-import java.awt.CardLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Toolkit;
+import java.awt.Color;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
 import javax.swing.JPasswordField;
 
 public class Kirjautuminen extends JPanel{
 
-
 	private static final long serialVersionUID = 1L;
 	private JButton kirjaudu, rekisteri;
-	private JTextField kayttaja;
 	private JPasswordField salasana;
-	private JLabel kayttajakentta, salasanakentta ;
-	private CardLayout cl;
-	private JPanel sailio;
+	private JLabel kayttajakentta, salasanakentta,nimimerkki, salasana1, salasana2, virhe, viesti, viesti2, kirjautumisohje, rekisteroitumisohje;
 	private JPanel kirjautuminen;
-	private Rekisteroituminen rekisteroituminen;
+	private JTextField nimimerkkikentta, kayttaja;
+	private JPasswordField salasanakentta1, salasanakentta2;
+	private Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 
 
 	public Kirjautuminen(){
-		// painikkeet
+
 		kirjaudu = new JButton("Kirjaudu sisään");
-		rekisteri = new JButton("Etkö ole rekisteröitynyt?");
-		// kirjoituskentät
+		rekisteri = new JButton("Rekisteröidy");
 		kayttaja = new JTextField();
 		salasana = new JPasswordField();
-		// tekstit
-		kayttajakentta = new JLabel("Käyttäjätunnus:");
+		salasanakentta1 = new JPasswordField();
+		salasanakentta2 = new JPasswordField();
+		kayttajakentta = new JLabel("Käyttäjänimi:");
 		salasanakentta = new JLabel("Salasana:");
-		cl = new CardLayout();
-		sailio = new JPanel(cl);
+		salasana1 = new JLabel("Anna salasana:");
+		salasana2 = new JLabel("Anna uudestaan:");
+		nimimerkkikentta = new JTextField();
+		nimimerkki = new JLabel("Anna käyttäjänimesi:");
 		kirjautuminen = new JPanel();
-		rekisteroituminen = new Rekisteroituminen();
-		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		viesti = new JLabel("Rekisteröityminen onnistui. Ole hyvä ja kirjaudu!");
+		kirjautumisohje = new JLabel("Kirjaudu sisään jos olet jo käyttäjä");
+		rekisteroitumisohje = new JLabel ("...tai luo ensin tunnus.");
+		viesti.setVisible(false);
 
-		//layout null, sammutus rastista, näkyvyys, koko jne
+		// fontit
+		Font fontti = new Font("Kumar One", Font.BOLD, 11);
+		Font otsikko = new Font("Kumar One", Font.BOLD, 15);
+
+
 		setSize(dim.width, dim.height);
 		setLayout(null);
 		setBackground(null);
 		setOpaque(false);
-		
-		//setLocation(0,0);
+		setVisible(true);
+
 
 		// Sisäänkirjautumisen actionlistener
 
@@ -61,13 +69,13 @@ public class Kirjautuminen extends JPanel{
 					int arvo = Integer.parseInt(tulos.substring(0, 1));
 					switch (arvo) {
 					case 0: //Kyseisellä kyselyllä ei ollut tulosta
-							System.out.println("Annettua yhdistelmää ei löydetty");
-							salasana.setText("");
+						System.out.println("Annettua yhdistelmää ei löydetty");
+						salasana.setText("");
 						break;
 					case 1:
-							System.out.println("Löydettiin");
-							Data.nimi = kayttaja.getText();
-							Data.vaihdaNakyma(Data.HAHMONAKYMA); //Pitää vaihtaa päänäkymään myöhemmin kun saadaan kaikki toimimaan
+						System.out.println("Löydettiin");
+						Data.nimi = kayttaja.getText();
+						Data.vaihdaNakyma(Data.HAHMONAKYMA); //Pitää vaihtaa päänäkymään myöhemmin kun saadaan kaikki toimimaan
 						break;			
 					default:
 						System.out.println(tulos);
@@ -82,36 +90,91 @@ public class Kirjautuminen extends JPanel{
 			}
 		}
 				);
-		// Jos käyttäjällä ei ole kirjautumisruudussa tunnuksia, hän voi
-		// siirtyä rekisteröitymisnäkymään
+
+		// Jos käyttäjällä ei ole kirjautumisruudussa tunnuksia, hän voi rekisteröityä
 
 		rekisteri.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){ 
-				cl.show(sailio, "reks");
+				if(String.valueOf(salasanakentta1.getPassword()).equals(String.valueOf(salasanakentta2.getPassword())) && String.valueOf(salasanakentta1.getPassword()) != null){
+					try {
+						Data.prp.rekisteroidu(nimimerkkikentta.getText(), String.valueOf(salasanakentta1.getPassword()));
+					} catch (RemoteException e1) {
+						e1.printStackTrace();
+					}
+					viesti.setVisible(true);
+					salasanakentta1.setText("");
+					salasanakentta2.setText("");
+					nimimerkkikentta.setText("");
+				}else{
+					virhe.setVisible(true);
+
+				}
 			}
 		}
 				);
-		// sijainti
-		kirjaudu.setBounds(140,55,200,20);
-		rekisteri.setBounds(140,75,200,20);
-		kayttaja.setBounds(140,10,200,20);
-		salasana.setBounds(140,30,200,20);
-		kayttajakentta.setBounds(10,10,120,20);
-		salasanakentta.setBounds(10,30,120,20);
 
-		add(sailio);
+		kirjaudu.setBounds(150,310,170,20);
+		rekisteri.setBounds(590,310,170,20);
+		kayttaja.setBounds(160,70,170,20);
+		salasana.setBounds(160,150,170,20);
+		kayttajakentta.setBounds(10,70,170,20);
+		salasanakentta.setBounds(10,150,120,20);
+		salasanakentta1.setBounds(590,150,170,20);
+		salasanakentta2.setBounds(590,230,170,20);
+		salasana1.setBounds(390,150,150,20);
+		salasana2.setBounds(390,230,150,20);
+		nimimerkkikentta.setBounds(590,70,170,20);
+		nimimerkki.setBounds(390,70,170,20);
+		viesti.setBounds(10,340,600,20);
+		kirjautumisohje.setBounds(10,15,400,20);
+		rekisteroitumisohje.setBounds(390,15,300,20);
+
 		add(kirjaudu);
 		add(rekisteri);
 		add(kayttaja);
 		add(salasana);
 		add(kayttajakentta);
+		add(nimimerkkikentta);
+		add(nimimerkki);
 		add(salasanakentta);
-		sailio.add(kirjautuminen, "kirj");
-		sailio.add(rekisteroituminen, "reks");
+		add(salasanakentta1);
+		add(salasanakentta2);
+		add(salasana1);
+		add(salasana2);
+		add(viesti);
+		add(kirjautumisohje);
+		add(rekisteroitumisohje);
+
+		nimimerkki.setFont(fontti);
+		salasana.setFont(fontti);
+		salasana1.setFont(fontti);
+		salasana2.setFont(fontti);
+		salasana.setFont(fontti);
+		viesti.setFont(fontti);
+		kayttaja.setFont(fontti);
+		salasanakentta.setFont(fontti);
+		kayttajakentta.setFont(fontti);
+		rekisteri.setFont(fontti);
+		kirjaudu.setFont(fontti);
+		nimimerkkikentta.setFont(fontti);
+		kirjautumisohje.setFont(otsikko);
+		rekisteroitumisohje.setFont(otsikko);
 
 
-		sailio.setVisible(true);
-		setVisible(true);
+		nimimerkki.setForeground(Color.PINK);
+		salasana.setForeground(Color.DARK_GRAY);
+		salasana1.setForeground(Color.PINK);
+		salasana2.setForeground(Color.PINK);
+		salasanakentta1.setForeground(Color.DARK_GRAY);
+		salasanakentta2.setForeground(Color.DARK_GRAY);
+		kayttajakentta.setForeground(Color.PINK);
+		kayttaja.setForeground(Color.DARK_GRAY);
+		nimimerkkikentta.setForeground(Color.DARK_GRAY);
+		salasanakentta.setForeground(Color.PINK);
+		kirjautumisohje.setForeground(Color.MAGENTA);
+		rekisteroitumisohje.setForeground(Color.MAGENTA);
+		viesti.setForeground(Color.MAGENTA);
+
 	}
 }
 
